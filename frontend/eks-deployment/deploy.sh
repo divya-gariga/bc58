187db2d69870c97b88b1b971d81daccbb9e3f872
divya-gariga <divya.gariga@zemosolabs.com>
2022-10-20 10:11:55 +0530

    updated build.yaml

--- frontend/eks-deployment/deploy.sh
+++ frontend/eks-deployment/deploy.sh
@@ -2,5 +2,5 @@ kubectl config set-context --current --namespace=bc58
 kubectl apply -f frontend/fe.deployment.yaml
 kubectl apply -f frontend/fe.service.yaml
 kubectl apply -f mockserver/ms.deployment.yaml
-kubectl apply -f frontend/ms.service.yaml
+kubectl apply -f mockserver/ms.service.yaml
 kubectl apply -f ingress.yaml
